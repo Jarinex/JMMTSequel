abd31d43f438d88857b4cd6dc5382a5a7b7ba568
Jarinex <31430822+Jarinex@users.noreply.github.com>
2021-10-29 18:50:22 -0600

    Add files via upload

--- WrathTweaksUnitReplacer.cs
+++ WrathTweaksUnitReplacer.cs
@@ -8,45 +8,51 @@ using System.Linq;
 using UnityEngine;
 using Kingmaker.PubSubSystem;
 using Kingmaker;
-
-
-
-
+using Kingmaker.EntitySystem.Entities;
 
 namespace WrathTweakMod
 {
 
 
-    class WrathUnitReplace
+   public class WrathUnitReplace
 
     {
 
-        struct UnitReplacement
+      public struct UnitReplacement
         {
-            string originalBlueprint;
-            BlueprintUnit replacementBlueprint;
+            public string originalBlueprint;
+            public BlueprintUnit replacementBlueprint;
         }
 
         public class UnitReplacementAreaHandler : IAreaHandler
         {
             private Dictionary<string, BlueprintUnit> replacements = new();
             private string areaID;
-            public void OnAreaLoadingComplete() //once the area is loaded, check for the original hydra
-            {
-                // Only do replacements if this is the correct area
-                if (Game.Instance.CurrentlyLoadedArea.AssetGuid.Equals(areaID))
-                {
-                    // Go through all units in the area
-                    foreach (var unit in Game.Instance.State.Units)
+            public void OnAreaDidLoad() //once the area is loaded, check for the original hydra
+               try {
+                    // Only do replacements if this is the correct area
+                    if (Game.Instance.CurrentlyLoadedArea.AssetGuid.Equals(areaID))
                     {
-                        //if we are aupposed to replace this unit...
-                        if (replacements.TryGetValue(unit.Blueprint.AssetGuid.ToString(), out string replacement))
+                        // Go through all units in the area
+                        foreach (var unit in Game.Instance.State.Units)
                         {
-                            // do it
-                            unit.ReplaceWith(replacement);
-                        }
-                    }
-                }
+                            //if we are aupposed to replace this unit...
+                            if (replacements.TryGetValue(unit.Blueprint.AssetGuid.ToString(), out string replacement))
+                            {
+                                // do it
+                                unit.ReplaceWith(replacement);
+                            }
+    }
+}
+                } catch (Exception ex)
+{
+    Main.DebugError(ex);
+}
+
+
+public void OnAreaBeginUnloading()
+            {
+
             }
 
             public UnitReplacementAreaHandler(string area, UnitReplacement[] replacementsArray)
@@ -59,18 +65,39 @@ namespace WrathTweakMod
 
         }
 
-        public static void ReplaceWith(this UnitEntityData toReplace, BlueprintUnit replaceWith)
-        {
-            var newUnit = Game.Instance.EntityCreator.SpawnUnit(replaceWith, toReplace.Position, Quaternion.LookRotation(toReplace.OrientationDirection), Game.Instance.CurrentScene.MainState);
-            newunit.GroupId = toReplace.GroupId;
-            toReplace.Destroy();
-        }
+
 
         public static void InstallUnitReplacer(string area, params UnitReplacement[] replacements)
         {
             EventBus.Subscribe(new UnitReplacementAreaHandler(area, replacements));
         }
 
+        public static string Test = "cd057d4d28be9fc46949fa1148bece7f";
+        public static string Firefly = "f8bf325a6020435e9719f56a4651b440";
+        public static string caves = " bad465e5404604346bdf71ef50b3299a";
+       
 
+        internal static void load()
+        {
+            InstallUnitReplacer(caves, new UnitReplacement
+            {
+                originalBlueprint = Test,
+                replacementBlueprint = Resources.GetBlueprint<BlueprintUnit>(Firefly)
+            });
+              
+        }
     }
+
+
+    public static class Jari
+    {
+        public static void ReplaceWith(this UnitEntityData toReplace, BlueprintUnit replaceWith)
+        {
+            var newUnit = Game.Instance.EntityCreator.SpawnUnit(replaceWith, toReplace.Position, Quaternion.LookRotation(toReplace.OrientationDirection), Game.Instance.State.LoadedAreaState.MainState);
+            newUnit.GroupId = toReplace.GroupId;
+            toReplace.Destroy();
+        }
+    }
+
 }
+
